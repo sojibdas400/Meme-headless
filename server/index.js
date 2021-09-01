22f025bdd0dbf971ff8f751797efe16585bc0f09
sojibdas400 <sojibdas400@gmail.com>
2021-09-02 00:50:08 +0600

    connecting to the database

--- server/index.js
+++ server/index.js
@@ -5,6 +5,21 @@ import cors from "cors";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://sojibdas:asdfghjkl@cluster0.qvsyq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
