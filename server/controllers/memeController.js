321252ed1d264fed302da83ca27911d1cbe516db
sojibdas400 <sojibdas400@gmail.com>
2021-09-02 02:58:57 +0600

    backend controllers

--- server/controllers/memeController.js
+++ server/controllers/memeController.js
@@ -1,3 +1,21 @@
-export const postMeme = (req,res)=>{
-    res.send("PostMeme")
-}
+import postMeme from "../models/postMeme.js";
+
+export const getMeme = async (req, res) => {
+  try {
+    const getAllMemes = await postMeme.find();
+    res.status(200).json(getAllMemes);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const createMeme = async (req, res) => {
+  const body = req.body;
+
+  const newMeme = new postMeme(body);
+  try {
+    await newMeme.save();
+    res.status(201).json(newMeme);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { postMeme } from "../controllers/memeController.js";
+import { getMeme, createMeme } from "../controllers/memeController.js";
 
 const router = express();
 
-router.get("/", postMeme);
+router.get("/", getMeme);
+router.get("/", createMeme);
 export default router;
